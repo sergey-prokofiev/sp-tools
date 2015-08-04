279c502f6b656d6a971983f70322d8100599af5c
Sergey Prokofiev <psergey79@gmail.com>
2015-08-04 13:45:52 +0300

    Automapper tests added

---
+++ sp-tools-tests/Wrappers/AutoMapperObjectMapperTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpTools.Wrappers;
+
+namespace sp_tools_tests.Wrappers
+{
+    [TestClass]
+    public class AutoMapperObjectMapperTests
+    {
+        class H1
+        {
+            public int Value { get; set; }
+        }
+        class H2
+        {
+            public int Value { get; set; }
+        }
+        [TestInitialize]
+        public void Init()
+        {
+            AutoMapper.Mapper.CreateMap<H1, H2>();
+        }
+        [TestMethod]
+        public void MapToNewTests()
+        {
+            var obj = new AutoMapperObjectMapper();
+            var result = obj.Map<H1, H2>(new H1 {Value = 42});
+            Assert.AreEqual(result.Value, 42);
+        }
+        [TestMethod]
+        public void MapToExistentTests()
+        {
+            var obj = new AutoMapperObjectMapper();
+            var result = new H2();
+            result = obj.Map(new H1 { Value = 42 }, result);
+            Assert.AreEqual(result.Value, 42);
+        }
+    }
+}
